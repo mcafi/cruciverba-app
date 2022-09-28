888e67acaa6533495c7ed5b6a74abceaad2ba64b
mcafi <matteo.carniglia@gmail.com>
2022-09-29 00:20:05 +0200

    la teoria c'è

--- pages/create.vue
+++ pages/create.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h1>Crea uno schema</h1>
-         <div class="flex" v-for="row in createBoard.board">
-            <div class="w-10 h-10 border-neutral-800 border-2" v-for="cell in row">
+        <div>
+            <button @click="createBoard.addRow">Aggiungi riga</button>
+            <button @click="createBoard.removeRow">Togli riga</button>
+        </div>
+        <div>
+            <button @click="createBoard.addColumn">Aggiungi colonna</button>
+            <button @click="createBoard.removeColumn">Togli colonna</button>
+        </div>
+        <div class="flex align-center justify-center p-20">
+            <div class="border-2 border-neutral-800 text-neutral-800">
+                <div class="flex" v-for="n, i in createBoard.height">
+                    <div v-for="m, j in createBoard.width" @click="createBoard.toggleCell(i, j)"
+                        class="w-10 h-10 border-neutral-800 border-2 cursor-pointer"
+                        :class="{'bg-neutral-800' : createBoard.board[i][j] == '#'}">
+                        {{createBoard.board[i][j]}}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">import { useCreateBoard } from '~/store/create';
-    const createBoard = useCreateBoard();
-    console.log(createBoard.board);
+const createBoard = useCreateBoard();
 </script>
--- store/create.ts
+++ store/create.ts
@@ -1,14 +1,45 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useCreateBoard = defineStore('createBoard', {
+export const useCreateBoard = defineStore("createBoard", {
   state: () => ({
-    board: new Array(40).fill('').map(() => new Array(30).fill('')),
+    board: new Array(100).fill("").map(() => new Array(100).fill("")),
+    width: 20,
+    height: 15,
   }),
-  // could also be defined as
-  // state: () => ({ count: 0 })
+  getters: {
+    cellsWithDefinitions: (state) => {
+      let defCounter = 1
+      for (let i = 0; i < state.width - 1; i++) {
+        for (let j = 0; j < state.height -1; j++) {
+        }
+      }
+    },
+    horizonrals: (state) => "",
+  },
   actions: {
-    increment() {
-      this.count++
+    toggleCell(x: number, y: number) {
+      if (this.board[x][y] == "#") this.board[x][y] = "";
+      else this.board[x][y] = "#";
+    },
+    addRow() {
+      this.height++;
+    },
+    removeRow() {
+      if (this.height > 2) {
+        this.height--;
+        this.board[this.height] = new Array(100).fill("");
+      }
+    },
+    addColumn() {
+      this.width++;
+    },
+    removeColumn() {
+      if (this.width > 2) {
+        this.width--;
+        this.board.forEach((row) => {
+          row[this.width] = "";
+        });
+      }
     },
   },
-})
+});
